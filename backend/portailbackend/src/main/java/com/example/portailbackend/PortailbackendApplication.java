70cb2f885c5cf9610c98f32b3675f2cbb80de8d6
HAMMI Yassine <yacine.hammi@esen.tn>
2025-01-25 20:02:07 +0100

    Arabsoft ftw

---
+++ backend/portailbackend/src/main/java/com/example/portailbackend/PortailbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.portailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailbackendApplication.class, args);
+	}
+
+}
---
+++ backend/portailbackend/src/test/java/com/example/portailbackend/PortailbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
